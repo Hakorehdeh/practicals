ae9b3c45e45742a81ccbba9e026c99c3e1427e99
Akorede <korederahman@gmail.com>
2019-04-04 10:58:25 +0100

    comment

---
+++ practicalclasswork.js
@@ -0,0 +1,9 @@
+var x = 7
+if (x===5) {
+ console.log("you have eaten the perfect amount of apples")
+} else if (x<5) {
+    console.log("you have eaten less than the perfect amount of apples")     
+} 
+else {
+    console.log("you have eaten greater than the perfect amount of apples")   
+} 
